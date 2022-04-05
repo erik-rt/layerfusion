ee60d33a9f388f77881b65b5d84368cfb89576ae
etash <erik.tastepe@gmail.com>
2022-04-05 15:41:15 -0400

    working on accepting folder on command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::path::PathBuf;
+use std::{env, fs};
+
+// Take in layers of art
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    #[clap(parse(from_os_str))]
+    path: PathBuf,
 }
+// Overlay layers atop one another and check that assets are not duplicated
+
+fn main() {
+    println!("We're generating some digital art");
+    let args = Cli::parse();
+    let paths = fs::read_dir(&args.path).unwrap();
+
+    for path in paths {
+        let out = path.unwrap().path().display().to_string();
+        println!("Name: {:?}", out.split("/").collect::<Vec<&str>>());
+    }
+    println!("{:?} and {:?}", args.pattern, args.path)
+}
+
+fn parse_args(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
+}
+
+fn generate_asset() {
+    println!("{:?}", env::current_dir())
+}
+
+// Generate metadata associated with each asset, compliant with OS
+
+// Update piece of metadata without regenerating assets
+
+// Wipe assets and regenerate everything
+
+// Add more assets to existing collection
