c07ea45729298351d9af90fc3bfc2cbdf4e201b6
etash <erik.tastepe@gmail.com>
2022-04-06 09:03:21 -0400

    added basic functionality to accept folder name argument

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use clap::Parser;
-use std::path::PathBuf;
-use std::{env, fs};
+// use failure::Error;
+use std::error::Error;
+use std::path::{Path, PathBuf};
+use std::{env, fs, process};
 
 // Take in layers of art
 #[derive(Parser)]
@@ -9,20 +11,53 @@ struct Cli {
     #[clap(parse(from_os_str))]
     path: PathBuf,
 }
-// Overlay layers atop one another and check that assets are not duplicated
 
-fn main() {
-    println!("We're generating some digital art");
-    let args = Cli::parse();
-    let paths = fs::read_dir(&args.path).unwrap();
-
-    for path in paths {
-        let out = path.unwrap().path().display().to_string();
-        println!("Name: {:?}", out.split("/").collect::<Vec<&str>>());
-    }
-    println!("{:?} and {:?}", args.pattern, args.path)
+struct Config {
+    dir: String,
 }
 
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("Not enough arguments");
+        }
+        let dir = args[1].clone();
+
+        Ok(Config { dir })
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("We're generating some digital art");
+    // let args = Cli::parse();
+    // let paths = fs::read_dir(args.path).unwrap();
+
+    // TODO: Write code that accepts folder instead of hardcoding a folder name
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    visit(Path::new(&config.dir))
+}
+
+fn visit(path: &Path) -> Result<(), Box<dyn Error>> {
+    for e in fs::read_dir(path)? {
+        let e = e?;
+        let path = e.path();
+        if path.is_dir() {
+            println!("Dir: {:?}", path);
+            visit(&path)?;
+        } else if path.is_file() {
+            println!("File: {:?}", path);
+        }
+    }
+    Ok(())
+}
+
+// Overlay layers atop one another and check that assets are not duplicated
 fn parse_args(args: &[String]) -> (&str, &str) {
     let query = &args[1];
     let filename = &args[2];
