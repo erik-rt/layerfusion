bd87c954515096dbd5eb2b9dd5e1b4f1918c5451
Erik T <erik.tastepe@gmail.com>
2023-07-27 19:57:13 -0400

    Added logic to load in layers, read file names, and create Layer objects

--- src/cli/generate.rs
+++ src/cli/generate.rs
@@ -55,17 +55,15 @@ impl Cmd for GenerateArgs {
             eyre::bail!("Directory {:?} does not exist", cwd)
         }
 
-        let input = Dir::read_dir(input)?;
-        let contents = input.contents;
+        let trait_layers = load_layers(input);
 
-        // TODO: Use iterator instead of imperative syntax
-
-        for c in contents {
-            let dir = fs::read_dir(c).unwrap();
-            for item in dir {
-                println!("{:?}", item.unwrap().path().display())
-            }
-        }
+        // for s in subdirs {
+        //     let dir = fs::read_dir(s)?;
+        //
+        //     for item in dir {
+        //         println!("{:?}", item.unwrap().path().display())
+        //     }
+        // }
 
         // Create the assets output folder if it does not exist
         if !assets.exists() {
@@ -76,34 +74,97 @@ impl Cmd for GenerateArgs {
         if !metadata.exists() {
             // fs::create_dir_all(&metadata)?;
         }
+
+        for i in 0..count {
+            todo!()
+        }
+
         Ok(())
     }
 }
 
-struct LayerData {
-    id: String,
+struct Attribute {
+    trait_type: String,
+    value: String,
+}
+
+#[derive(Debug)]
+struct Layer {
+    trait_type: String,
     trait_name: String,
     rarity: u32,
 }
 
-fn create_artwork(layers: &[&Arc<LayerData>]) {
+// impl Layer {
+//     fn new(self, id: u32, trait_name: String, rarity: u32) -> Self {
+//         Layer {
+//             id,
+//             trait_name,
+//             rarity,
+//         }
+//     }
+// }
+
+fn create_artwork(layers: &[&Arc<Layer>]) {
     todo!()
 }
 
-fn encode_combination(layers: &[&Arc<LayerData>]) -> eyre::Result<String> {
-    Ok(layers
-        .iter()
-        .map(|layer| layer.id.to_string())
-        .collect::<Vec<_>>()
-        .join("-"))
-}
-
-type ArtworkLayers = Vec<Vec<Arc<LayerData>>>;
-
-fn load_layers(input: PathBuf) -> eyre::Result<ArtworkLayers> {
+fn encode_combination(layers: &[&Arc<Layer>]) -> eyre::Result<String> {
     todo!()
 }
 
-fn generate_combinations(layers: &[&Arc<LayerData>]) -> eyre::Result<()> {
+type ArtLayers = Vec<Vec<Box<Layer>>>;
+
+fn load_layers(input_dir: PathBuf) -> eyre::Result<ArtLayers> {
+    let subdirs = Dir::read_dir(input_dir)?.contents;
+
+    // This flow actually might be better written imperatively so that error handling is easier
+    let trait_layers = subdirs
+        .into_iter()
+        .map(|subdir| {
+            let trait_type = subdir.file_stem().unwrap();
+
+            // Logic for removing the layer order prefix. Still figuring out the order of
+            // operations of this code.
+            // If this is included, the `trait_type` Layer field needs to be updated to
+            // `trait_type.to_owned().into_string().unwrap()`
+            // let trait_type = trait_type.to_owned().into_string().unwrap();
+            // let [_, trait_type]: [&str; 2] = trait_type
+            //     .split("_")
+            //     .collect::<Vec<&str>>()
+            //     .try_into()
+            //     .unwrap();
+
+            let subdir = fs::read_dir(&subdir).unwrap();
+            subdir
+                .into_iter()
+                .map(|file| {
+                    // Get the file from within each subdirectory
+                    let file = file.unwrap().file_name();
+                    // Create a Path from the file
+                    let file_path = Path::new(&file);
+                    // Get the stem from the Path and convert it to a String
+                    let file_stem = file_path
+                        .file_stem()
+                        .unwrap()
+                        .to_owned()
+                        .into_string()
+                        .unwrap();
+
+                    // Create a new Arc<Layer> and return
+                    Box::new(Layer {
+                        trait_type: trait_type.to_owned().into_string().unwrap(),
+                        trait_name: file_stem,
+                        rarity: 1,
+                    })
+                })
+                .collect::<Vec<Box<Layer>>>()
+        })
+        .collect::<ArtLayers>();
+
+    Ok(trait_layers)
+}
+
+fn generate_combinations(layers: &[&Arc<Layer>]) -> eyre::Result<()> {
     todo!()
 }
